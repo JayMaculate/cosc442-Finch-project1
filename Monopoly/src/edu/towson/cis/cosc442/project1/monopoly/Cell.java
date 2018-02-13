b26ce2023d54ef2b5d3a5bc0858548de3f3fd72e
JayMaculate <joshuafinch@192.168.1.13>
2018-02-13 17:32:44 -0500

    Task 10 - 12 Are complete in this commit after running the JUNIT

--- Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ Monopoly/src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -1,41 +1,49 @@
 package edu.towson.cis.cosc442.project1.monopoly;
 
-public abstract class Cell {
-	private boolean available = true;
+public abstract class Cell implements IOwnable {
 	private String name;
-	protected Player owner;
+	protected Player theOwner;
+	private boolean available = true;
 
 	public String getName() {
 		return name;
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.towson.cis.cosc442.project1.monopoly.IOwnable#getOwner()
+	 */
+	@Override
 	public Player getOwner() {
-		return owner;
+		return theOwner;
 	}
 	
 	public int getPrice() {
 		return 0;
 	}
 
-	public boolean isAvailable() {
-		return available;
-	}
-	
 	public abstract void playAction();
 
-	public void setAvailable(boolean available) {
-		this.available = available;
-	}
-	
 	void setName(String name) {
 		this.name = name;
 	}
 
+	/* (non-Javadoc)
+	 * @see edu.towson.cis.cosc442.project1.monopoly.IOwnable#setOwner(edu.towson.cis.cosc442.project1.monopoly.Player)
+	 */
+	@Override
 	public void setOwner(Player owner) {
-		this.owner = owner;
+		this.theOwner = owner;
 	}
     
     public String toString() {
         return name;
     }
+
+	public boolean isAvailable() {
+		return available;
+	}
+
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
 }
